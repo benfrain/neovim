2d020b506fded04fe2717994389bf941286a4e9b
Ben Frain <bf@benfrain.com>
2024-10-11 12:08:42 +0100

    Update fzf.lua
    better defaults to wrap the display

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,7 +1,14 @@
 require("fzf-lua").setup({
+  fzf_opts = { ["--wrap"] = true },
+  winopts = {
+    preview = {
+      wrap = "wrap",
+    },
+  },
   defaults = {
     git_icons = false,
     file_icons = false,
     color_icons = false,
+    formatter = "path.filename_first",
   },
 })
