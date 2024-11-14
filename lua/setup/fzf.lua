bd0f15ac6569d300620899eea4fc0671cc06fc95
Ben Frain <bf@benfrain.com>
2024-11-14 16:35:53 +0000

    Update fzf.lua
    Adding ability to use flags

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,6 +1,16 @@
 require("fzf-lua").setup({
   fzf_opts = { ["--wrap"] = true },
-  grep = { rg_glob = true },
+  grep = {
+    rg_glob = true,
+    -- first returned string is the new search query
+    -- second returned string are (optional) additional rg flags
+    -- @return string, string?
+    rg_glob_fn = function(query, opts)
+      local regex, flags = query:match("^(.-)%s%-%-(.*)$")
+      -- If no separator is detected will return the original query
+      return (regex or query), flags
+    end,
+  },
   winopts = {
     preview = {
       wrap = "wrap",
