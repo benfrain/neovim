72ca8425e2358ef644fc670722691c2c658784a2
Ben Frain <bf@benfrain.com>
2024-12-12 16:25:57 +0000

    Update treesitter.lua
    tidy up

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -1,9 +1,4 @@
 require("nvim-treesitter.configs").setup({
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
   autotag = {
     enable = true,
     filetypes = {
@@ -13,19 +8,6 @@ require("nvim-treesitter.configs").setup({
       "markdown",
     },
   },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<CR>",
-      scope_incremental = "<CR>",
-      node_incremental = "<CR>",
-      node_decremental = "<TAB>",
-    },
-  },
-})
--- Setup treesitter
-local ts = require("nvim-treesitter.configs")
-ts.setup({
   ensure_installed = {
     "c",
     "css",
@@ -42,4 +24,13 @@ ts.setup({
     "toml",
   },
   highlight = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<CR>",
+      node_decremental = "<TAB>",
+    },
+  },
 })
