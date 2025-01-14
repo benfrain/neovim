c69bbf55130744391eec55d980e2dc73f5ebf5f4
Ben Frain <bf@benfrain.com>
2025-01-14 09:37:58 +0000

    Update fzf.lua
    Adding custom action to enable copying the filename and path of a result to the clipboard

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,3 +1,18 @@
+local path = require("fzf-lua.path")
+get_line_and_path = function(selected, opts)
+  local file_and_path = path.entry_to_file(selected[1], opts).stripped
+  vim.print(file_and_path)
+  if vim.o.clipboard == "unnamed" then
+    vim.fn.setreg([[*]], file_and_path)
+  elseif vim.o.clipboard == "unnamedplus" then
+    vim.fn.setreg([[+]], file_and_path)
+  else
+    vim.fn.setreg([["]], file_and_path)
+  end
+  -- copy to the yank register regardless
+  vim.fn.setreg([[0]], file_and_path)
+end
+
 require("fzf-lua").setup({
   "border-fused",
   fzf_opts = { ["--wrap"] = true },
@@ -27,5 +42,10 @@ require("fzf-lua").setup({
     file_icons = false,
     color_icons = false,
     formatter = "path.filename_first",
+  },
+  actions = {
+    files = {
+      ["ctrl-y"] = { fn = get_line_and_path, exec_silent = true },
+    },
   },
 })
